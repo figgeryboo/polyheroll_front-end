ee8fea90dabf6216b91c04fca693d0b6b626c965
Fig <bethanytrr@gmail.com>
2023-11-21 02:18:03 -0500

    Created Front End app, initiated vite structure and began to settle folders/pages/components.

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+//import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import List from "./pages/List";
+import New from "./pages/New";
+import Show from "./pages/ShowPage";
+import Edit from "./pages/EditPage";
+import Error404 from "./pages/Error404Page"
+import './global.css'
+
+function App() {
+
+	return (
+		<div className="App">
+			<Router>
+				<main>
+					<Routes>
+						<Route path="/" element={<Home />} />
+					</Routes>
+				</main>
+			</Router>
+		</div>
+	);
+}
+
+export default App;
